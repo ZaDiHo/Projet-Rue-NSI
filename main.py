fe1c623f3f111b32e2a12c4897a6d83892ee5882
ZaDiHo <contact@thomasprovost.fr>
2023-09-12 14:57:00 +0200

    🌲| Création de l'environnement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import yaml
+from yaml import Loader
 
 #
 # * Nom de classe : main.py
@@ -8,8 +10,21 @@ from tkinter import *
 # * Auteur        : Thomas PROVOST
 #
 
+# Accès au fichier de configuration config.yml
+with open("config.yml", "r") as ymlfile:
+    config = yaml.load(ymlfile, Loader=Loader)
+
 # Création de la fenêtre principale
 window = Tk()
 window.title("Projet Rue NSI - Thomas PROVOST")
 window.geometry("1920x1080")
+window.config(background=config["global"]["background_color"])
+
+# Création du sol
+ground = Canvas(window, width=1920, height=200, highlightthickness=0, bg=config["global"]["ground_color"])
+ground.pack(side="bottom")
+
 window.mainloop()
+
+ground = Canvas(window, width=1920, height=200, highlightthickness=0, bg=config["global"]["background_color"])
+ground.pack(side="bottom")
