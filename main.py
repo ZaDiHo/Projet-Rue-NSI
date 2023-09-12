43cae1076bdde30f8034c116e71e87f27f8c160f
ZaDiHo <contact@thomasprovost.fr>
2023-09-12 14:32:18 +0200

    👍| Creation of the main class

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+#
+# * Nom de classe : main.py
+# * Description   : Class principale du projet
+# * Version       : 0.1
+# * Date          : 12/09/2023
+# * Auteur        : Thomas PROVOST
+#
+
+# Création de la fenêtre principale
+window = Tk()
+window.title("Projet Rue NSI - Thomas PROVOST")
+window.geometry("1920x1080")
+window.mainloop()
